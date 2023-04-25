d8d6518b755eb9f27d106309a117821d5a1c4a00
Marco Arath <marco.martinez.morales@u-erre.mx>
2023-04-25 17:01:46 -0600

    completada sesion 2 de javascript

--- js02/js02.js
+++ js02/js02.js
@@ -140,7 +140,7 @@ function addH1(mensaje) {
 } 
 
 printToConsole("Hola, mundo!");
-showAlert("¡Bienvenido!");
+//showAlert("¡Bienvenido!"); // bienvenido alerta
 
  // GET NAME
 
@@ -154,19 +154,155 @@ showAlert("¡Bienvenido!");
     const text = `Hola ${name}, espero tengas un gran dia`;
     addH1(text);
  }
- function getNamePrint( opcion ){ // esta es la buena
+ function getNamePrint( funcionImprimir ){ // esta es la buena
     const name = prompt("Dime tu nombre");
     const text = `Hola ${name}, espero tengas un gran dia`;
-    if (opcion === 1)
-        printToConsole(text);
-    else if(opcion === 2)
-        addH1(text);
-    else
-    showAlert(text);
+    funcionImprimir(text);
  }
 
-getNamePrint(1);
-getNamePrint(2);
-getNamePrint(3);
+/* getNamePrint( printToConsole ); // pasando referencia de la funcion porque no tiene()
+getNamePrint( showAlert );
+getNamePrint( addH1 ); */
  // getNamePrintToConsole();
  // getNamePrintToDom();
+
+ // -------------------------------------
+/*
+    Ejercicio 2
+    Escribe una función que tome un arreglo de números,
+    duplique el valor de cada número del arreglo,
+    e imprima el nuevo arreglo actualizado.
+    [5, 10, 15, 20, 25] -> [10, 20, 30, 40, 50]
+*/
+
+function double(arr) {
+    const doubleNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+      const duplicate = arr[i] * 2;
+      doubleNumbers.push(duplicate);
+    }
+   return doubleNumbers;
+}
+
+// Función que nos sirve de callback
+// const duplicate = (number) => number * 2;
+/*
+function doubleWithMap( array ){
+    const duplicateNumber = array.map( function(number){
+        return number * 2;
+    }  );
+    return duplicateNumber;
+}*/
+
+function doubleWithMap( array ){
+   return array.map( number=> number * 2 );    
+}
+
+
+const numbers = [5, 10, 15, 20, 25]; 
+//console.log(double(numbers));
+console.log(doubleWithMap(numbers));
+console.log( numbers.map(number => number * 2)); // numbers.map()
+console.log(numbers);
+
+//
+/*
+  Ejercicio 4
+  Crear un programa que itere sobre dos arreglos;
+  si hay cursos en común, imprimirlos en la consola.
+  salida: "Cursos en común: Programming, Music"
+*/
+
+
+
+function cursosEnComun (student1Courses,student2Courses) {
+    const courses = [];
+    for (let i=0; i < student1Courses.length; i++) {
+     for (let j=0; j < student2Courses.length; j++) {
+      if (student1Courses[i] == student2Courses[j]){
+        courses.push( student1Courses[i] );
+      }
+     }
+    }
+    return `Cursos en común: ${courses}`;     
+  }
+
+// --------------------- Usando método filter e includes
+
+const student1Courses = ["Math", "English", "Programming", "Biology", "Physics", "Music"];
+const student2Courses = ["Geography", "Spanish", "Programming", "Music"];
+const student3Courses = ["Music"];
+
+function getCommonCoursesWithFilter(student1Courses,student2Courses) {
+    return student1Courses.filter(course => student2Courses.includes(course));
+}
+
+const commonCourses = getCommonCoursesWithFilter(student1Courses, student2Courses);
+const commonCoursesWithStudent3 = getCommonCoursesWithFilter( commonCourses, student3Courses)
+console.log("Common courses:", commonCoursesWithStudent3.join(", "));
+
+// ------------------------------------
+// Contar la cantidad de caracteres de una frase
+// pepe pecas pica papas con un pico y una pala
+// cantidad de letras p:8
+// resolverlo usando arrow function
+
+phrase = "pepe pecas pica papas con un pico y una pala";
+
+const countChar = (p, phrase) => phrase.split("").filter( c => c == p).length;
+console.log("Cantidad de letras 'p': ", countChar('p', phrase));
+
+// -------------- Funciones recursivas -------------------
+// Es una tecnica de programacion en donde la funcion se llama a si misma
+// Se debe tener precaucion de no entrar a un bucle infinito
+
+/*
+    function funcionRecursiva(valor ){
+        if ( condicionDeParo ){
+
+        }
+        else {
+            // Llamada recursiva
+        }
+    }
+*/
+
+const factorialConCicloFor = numero =>{
+    let factorial = 1;
+    for(let i = numero; i > 0; i-- ){
+        factorial *= i; // factorial = factorial * i;
+    }
+    return factorial;
+}
+
+function factorialRecursivo ( numero ) {
+    if( numero === 1){
+        return 1;
+    }
+    return numero * factorialRecursivo( numero -1);
+}
+
+console.log("El factorial de 5: " + factorialConCicloFor(5));
+console.log("El factorial de 5: " + factorialRecursivo(5));
+
+// ------------ Ejercicio ---------------------------------
+// Una función recursiva que muestre en consola
+/*
+Saludo 1
+Saludo 2
+Saludo 3
+ ...
+Saludo 10
+
+saludo(10);
+*/
+
+function saludos(numeroSaludo ){
+    if ( numeroSaludo > 1 ){
+        saludos(numeroSaludo -1);
+    }
+        console.log("Saludo " + numeroSaludo);
+}
+
+saludos(10);
+
