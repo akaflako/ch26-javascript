a557fe36413d995b9973657794aac65522129ba9
Marco Arath <marco.martinez.morales@u-erre.mx>
2023-04-25 11:03:31 -0600

    funciones callback

--- js02/js02.js
+++ js02/js02.js
@@ -94,3 +94,79 @@ const areaTriangulo = (base=1 , altura=1) => base * altura / 2;
 
 console.log("Area de triangulo b:5 a:10 = " + areaTriangulo(5,10) ); // 25
 console.log("Area de triangulo b:8 = " + areaTriangulo(8) ); // 4
+
+//---------- Parametros rest -----------------
+// Rest Parameters
+// Nos permite representar una serie de valores indefinidos en los argumentos
+// Estos se representan como un array
+
+function sumatoriaVariosNumeros(a , b, ...restoDatos ){
+    let suma;
+    suma = a + b;
+    for (let index = 0; index < restoDatos.length; index++) {
+        suma += restoDatos[index]; // suma = suma + restoDatos[index]
+    } 
+
+    return suma;
+}
+
+console.log("Sumatoria de varios numeros: " + sumatoriaVariosNumeros(5, 6) ); // 11
+console.log("Sumatoria de varios numeros: " + sumatoriaVariosNumeros(5, 6, 9, 10) ); // 30
+console.log("Sumatoria: " + sumatoriaVariosNumeros(5, 6, 9, 10, 20, 50) ); // 100
+
+// ---------- Funciones CallBack --------------------
+// Funcion que se pasa a otra funcion como argumento, para luego invocarse
+// para completar algun tipo de rutina o accion
+
+// 3 funciones, una que imprima en consola, alert, DOM h1.
+// 1 parametro de entrada
+// no tienen return
+
+// Función que imprime un mensaje en la consola
+function printToConsole(mensaje) {
+    console.log(mensaje);
+  }
+  
+// Función que muestra una alerta en la pantalla
+function showAlert(mensaje) {
+    alert(mensaje);
+  }
+  
+// Función que agrega un encabezado h1 al DOM
+function addH1(mensaje) {
+    const h1 = document.createElement("h1");
+    h1.innerText = mensaje;
+    document.body.appendChild(h1);
+} 
+
+printToConsole("Hola, mundo!");
+showAlert("¡Bienvenido!");
+
+ // GET NAME
+
+ function getNamePrintToConsole( ){
+    const name = prompt("Dime tu nombre");
+    const text = `Hola ${name}, espero tengas un gran dia`;
+    printToConsole(text);
+ }
+ function getNamePrintToDom( ){
+    const name = prompt("Dime tu nombre");
+    const text = `Hola ${name}, espero tengas un gran dia`;
+    addH1(text);
+ }
+ function getNamePrint( opcion ){ // esta es la buena
+    const name = prompt("Dime tu nombre");
+    const text = `Hola ${name}, espero tengas un gran dia`;
+    if (opcion === 1)
+        printToConsole(text);
+    else if(opcion === 2)
+        addH1(text);
+    else
+    showAlert(text);
+ }
+
+getNamePrint(1);
+getNamePrint(2);
+getNamePrint(3);
+ // getNamePrintToConsole();
+ // getNamePrintToDom();
